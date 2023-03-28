80db0c8c4c41fad0ee06a3c973fe59c77a863922
redraincatching <redraincatching@disroot.org>
2023-03-28 17:51:43 +0100

    added 'because you liked'

--- src/components/RecommenderBody.vue
+++ src/components/RecommenderBody.vue
@@ -39,9 +39,9 @@ export default {
         else {
             this.loggedIn = false;
         }
-        
+
     },
-    mounted(){
+    mounted() {
         this.loggedIn = isLoggedIn();
     },
     // watch: {
@@ -52,19 +52,20 @@ export default {
     // },
     methods: {
         refreshRecommendation() {
-            if(!this.loading){
+            if (!this.loading) {
                 startLoad(this);
                 this.loading = true;
             }
-            
+
             recommend({ token: this.token, user: getProfileInfo() }).then((recommendation) => {
                 console.log(recommendation.data);
-                if(recentlySuggested(recommendation.data.artist)){  // || recommendation.data.code == 1
+                if (recentlySuggested(recommendation.data.artist)) {  // || recommendation.data.code == 1
                     this.refreshRecommendation();
                 }
-                else{
+                else {
                     this.currentSuggestion = recommendation.data.artist;
                     this.genres = "";
+                    this.similarArtist = recommendation.data.similar[Math.floor(Math.random() * 12345) % recommendation.similar.length];
                     this.currentSuggestion.genres.forEach((genre) => {
                         this.genres += genre + ' , ';
                     });
@@ -75,35 +76,18 @@ export default {
         },
 
         likeArtist() {
-            if(this.currentSuggestion != null){
+            if (this.currentSuggestion != null) {
                 let currProfileInfo = getProfileInfo(); //make a copy of the current profile information
                 console.log(currProfileInfo);
                 currProfileInfo.likedArtists.push(this.currentSuggestion);
                 let newData = currProfileInfo.likedArtists;
-    
-                
+
+
                 setProfileInfo(currProfileInfo);
                 updateSuggestedArtists(this.currentSuggestion);
-                
-                if(newData !== null){
-                    update({ field: 'likedArtists', value: newData}).then((result) => {
-                        console.log(result.data);
-                        this.refreshRecommendation();
-                    }).catch((error) => {
-                        console.log(error);
-                    });
-                }
-            }          
-        },
 
-        skipArtist(){
-            if(this.currentSuggestion != null){
-                updateSuggestedArtists(this.currentSuggestion);
-    
-                let newData = getProfileInfo().suggestedArtists;
-                
-                if(newData !== null){
-                    update({ field: 'suggestedArtists', value: newData}).then((result) => {
+                if (newData !== null) {
+                    update({ field: 'likedArtists', value: newData }).then((result) => {
                         console.log(result.data);
                         this.refreshRecommendation();
                     }).catch((error) => {
@@ -113,16 +97,33 @@ export default {
             }
         },
 
-        opensignin(){
+        skipArtist() {
+            if (this.currentSuggestion != null) {
+                updateSuggestedArtists(this.currentSuggestion);
+
+                let newData = getProfileInfo().suggestedArtists;
+
+                if (newData !== null) {
+                    update({ field: 'suggestedArtists', value: newData }).then((result) => {
+                        console.log(result.data);
+                        this.refreshRecommendation();
+                    }).catch((error) => {
+                        console.log(error);
+                    });
+                }
+            }
+        },
+
+        opensignin() {
             openModal(0);
         },
 
-        opensignup(){
+        opensignup() {
             openModal(1);
         }
     },
     computed: {
-        artistImage(){
+        artistImage() {
             return JSON.stringify(this.currentSuggestion) != '{}' ? this.currentSuggestion.images[0].url : "";
         }
     }
@@ -131,43 +132,46 @@ export default {
 </script>
 
 <template >
-
-
     <div v-if="loggedIn">
         <section class="px-4 py-5 text-center" style="width: 100vw; height: 100vh; color:white; background-color:black">
-    <h1 class="display-5 fw-bold" style="padding-top:10%">Artist: {{ this.currentSuggestion.name }}</h1>
-    <div class="col-lg-6 mx-auto">
-      <p class="lead mb-4">Genres: {{ this.genres }}</p>
-
-        
-      <div class="container px-1">
-          <button style="background-color:black; padding-left:15px; border: none; font-size:150%"   @click="skipArtist">&#10060;</button>
-            <img :src="artistImage" :alt="this.currentSuggestion.name" class="img-fluid rounded-3 " width="180" height="180" loading="lazy">
-<button style="background-color:black; padding-right:15px; border: none; font-size:150%" @click="likeArtist">&#9989;</button>
-          
-      </div>
+            <h1 class="display-5 fw-bold" style="padding-top:10%">Artist: {{ this.currentSuggestion.name }}</h1>
+            <h2 class="display-5 fw-bold" style="padding-top:10%"> Because you liked {{ this.similarArtist }} </h2>
+            <div class="col-lg-6 mx-auto">
+                <p class="lead mb-4">Genres: {{ this.genres }}</p>
 
 
+                <div class="container px-1">
+                    <button style="background-color:black; padding-left:15px; border: none; font-size:150%"
+                        @click="skipArtist">&#10060;</button>
+                    <img :src="artistImage" :alt="this.currentSuggestion.name" class="img-fluid rounded-3 " width="180"
+                        height="180" loading="lazy">
+                    <button style="background-color:black; padding-right:15px; border: none; font-size:150%"
+                        @click="likeArtist">&#9989;</button>
 
-      
-      
-    </div>
-  </section>
-    
+                </div>
+
+
+
+
+
+            </div>
+        </section>
+
     </div>
     <div v-else>
         <section class="px-4 py-5 text-center" style="width: 100vw; height: 100vh; color:white; background-color:black">
 
-      <div class="col-lg-6 mx-auto" style="padding-top:10%">
-        <h4 class="lead mb-4" style="color:white; ">You are not logged into your account. To login click <h3
-            style="display:inline"><strong><a @click="opensignin()">here</a></strong></h3> or to create an account click
-          <h3 style="display:inline"><strong><a @click="opensignup()">here</a></strong></h3>
-        </h4>
-        <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+            <div class="col-lg-6 mx-auto" style="padding-top:10%">
+                <h4 class="lead mb-4" style="color:white; ">You are not logged into your account. To login click <h3
+                        style="display:inline"><strong><a @click="opensignin()">here</a></strong></h3> or to create an
+                    account click
+                    <h3 style="display:inline"><strong><a @click="opensignup()">here</a></strong></h3>
+                </h4>
+                <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
 
-        </div>
-      </div>
-    </section>
+                </div>
+            </div>
+        </section>
     </div>
 </template>
 
@@ -177,6 +181,7 @@ export default {
     background-color: black;
     padding-top: 12.5%;
 }
+
 .btn-get-started {
     font-weight: 500;
     font-size: 16px;
@@ -190,15 +195,18 @@ export default {
     text-decoration: none;
     border: 2px solid #1DB954;
 }
+
 .btn-get-started:hover {
     background: #1DB954;
     color: black;
 }
+
 .artistName {
     color: white;
     font-size: 100px;
     text-align: center;
 }
+
 .artistGenre {
     color: white;
     font-size: 20px;
